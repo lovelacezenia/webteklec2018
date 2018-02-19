cfd51c0da3ca278727bf6c7f59184eec88f12d42
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-02-20 00:18:42 +0800

    update structure of acronyms

--- webteklec 2018/assets/js/script.js
+++ webteklec 2018/assets/js/script.js
@@ -1,14 +1,16 @@
-var $ = jQuery;
-jQuery(document).ready(function(){
-	
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("searchinput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("search");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
 
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
-
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
-
-})
+        }
+    }
+}
